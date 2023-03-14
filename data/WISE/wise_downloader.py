581e37ab6018a794aa202835e2bb4c4c3acad991
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-03-14 13:55:12 -0300

    implements wise downloader

--- data/WISE/wise_downloader.py
+++ data/WISE/wise_downloader.py
@@ -5,6 +5,7 @@ import tarfile
 import os
 import multiprocessing
 import tqdm
+import shutil
 
 WISE_FOLDER = "./wise_fits"
 W1_FOLDER = "./wise_fits/w1"
@@ -18,25 +19,32 @@ ID = "ID"
 def wise_url(ra, dec, size):
     return f"http://unwise.me/cutout_fits?version=allwise&ra={ra}&dec={dec}&size={size}&bands=12&file_img_m=on"
 
-def download(link, filelocation):
+def download(link, folder, file):
     # from https://stackoverflow.com/questions/33541956/how-to-download-a-file-over-http-with-multi-thread-asynchronous-download-using
     r = requests.get(link, stream=True)
+    filelocation = folder + "/" + file + ".tar.gz"
 
     with open(filelocation, 'wb') as f:
         f.write(r.content)
 
     tar = tarfile.open(filelocation)
     names = tar.getnames()
-    tar.extract(names[0], W1_FOLDER)
-    tar.extract(names[1], W2_FOLDER)
 
-    os.remove(filelocation)
+    tar.extractall(folder)
+
+    if "w1" not in names[0]: print("ARQUIVO EM PASTA ERRADA")
+    shutil.move(folder + "/" + names[0],W1_FOLDER + "/" + file + ".fits")
+    shutil.move(folder + "/" + names[1],W2_FOLDER + "/" + file + ".fits")
     return 
 
 def get_fits(ra_dec_size_id):
     (ra, dec, size, id) = ra_dec_size_id
     url = wise_url(ra,dec,size)
-    download(url, WISE_FOLDER + f"/{id}.tar.gz")
+
+    folder = WISE_FOLDER + "/" + id
+    os.mkdir(folder)
+    download(url, folder, f"{id}")
+    shutil.rmtree(folder)
 
 
 def main():
@@ -49,7 +57,7 @@ def main():
     SIZEs = [max(32, 3*(csv['FWHM_n'].iloc[i])) for i in range(len(csv))]
     IDs = list(csv[ID])
 
-    ra_dec_size_id = [(RAs[i], DECs[i], SIZEs[i], IDs[i]) for i in range(len(csv))][2:4]
+    ra_dec_size_id = [(RAs[i], DECs[i], SIZEs[i], IDs[i]) for i in range(len(csv))][0:10]
 
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
         with tqdm.tqdm(total=len(ra_dec_size_id)) as pbar:
